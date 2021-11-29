4d39f6a68e5feca6ec07d7aec449d90846638e01
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2021-11-29 23:00:39 +0600

    redux added

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './slices/counterSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import counterReducer from './slices/counterSlice';
+
+const RootReducer = combineReducers({
+    counter: counterReducer
+})
 
 export const store = configureStore({
-    reducer: {
-        counter: counterReducer
-    },
+    reducer: RootReducer
   })
